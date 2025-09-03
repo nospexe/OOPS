7e4ce521340d0435da757d35c36fd70578b7008b
nospexe <aayush.r0412@gmail.com>
2025-09-03 09:45:26 +0530

    Adding intro c++ syntax files

---
+++ intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string x;
+    cout <<"Hello" << endl;
+    cout <<"Please enter a name \n";
+    cin>> x;
+    cout << x << " is the name!!!";
+    return 0;
+}
